14c4c7d84c514aafd71c15116bad095917445acb
ShreyasRede <105453060+ShreyasRede@users.noreply.github.com>
2024-10-18 13:17:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title('⚡Electricity Price Prediction')
 
 st.info('Suppose that your business relies on computing services where the power consumed by your machines varies throughout the day. You do not know the actual cost of the electricity consumed by the machines throughout the day, but the organization has provided you with historical data of the price of the electricity consumed by the machines.')
 
-d = st.date_input("Select The Date", format="DD/MM/YYYY")
+d = st.date_input("Select The Date", format="DD/MM")
 st.write(d)
