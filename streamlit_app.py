272cb281fcfb12bc74f467d48f8918c369a863b3
ShreyasRede <105453060+ShreyasRede@users.noreply.github.com>
2024-10-18 15:25:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,24 @@ st.title('⚡Electricity Price Prediction')
 
 st.info('Suppose that your business relies on computing services where the power consumed by your machines varies throughout the day. You do not know the actual cost of the electricity consumed by the machines throughout the day, but the organization has provided you with historical data of the price of the electricity consumed by the machines.')
 
+data = data.dropna()
+
+
+x = data[["Day", "Month", "ForecastWindProduction", "SystemLoadEA", 
+          "SMPEA", "ORKTemperature", "ORKWindspeed", "CO2Intensity", 
+          "ActualWindProduction", "SystemLoadEP2"]]
+y = data["SMPEP2"]
+
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y,  test_size=0.2,random_state=42)
+
+
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor()
+model.fit(xtrain, ytrain)
+
+#input features
 Date =st.slider('Select a Date', min_value=1, max_value=31)
 Month =st.slider('Select a Month', min_value=1, max_value=12)
 st.write("you've selected date is",Date,"/",Month)
@@ -20,15 +38,6 @@ CO2Intensity= st.number_input("CO2Intensity")
 ActualWindProduction= st.number_input("ActualWindProduction")
 SystemLoadEP2= st.number_input("SystemLoadEP2")
 if st.button("submit"):
-    data = {'Day':Date,
-            'Month':Month,
-            'ForecastWindProduction':ForecastWindProduction,
-            'SystemLoadEA':SystemLoadEA,
-            'SMPEA':SMPEA,
-            'ORKTemperature':ORKTemperature,
-            'ORKWindspeed':ORKWindspeed,
-            'CO2Intensity':CO2Intensity,
-            'ActualWindProduction':ActualWindProduction,
-            'SystemLoadEP2':SystemLoadEP2}
-    input_data=pd.DataFrame(data,index=[0])
-    st.write(input_data)
+    data=[[Date, Month, ForecastWindProduction, SystemLoadEA, SMPEA, ORKTemperature, ORKWindspeed, CO2Intensity, ActualWindProduction, SystemLoadEP2]]
+    prediction=model.predict(features)
+    st.info(prediction)
